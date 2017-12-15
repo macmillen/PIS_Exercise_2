ef6435211601861978f8bf830c23248fb672fb0b
dswn38 <dominik.schwan@mni.thm.de>
2017-12-15 16:41:58 +0100

    ServerGUI Structure

--- src/pis/hue2/server/ServerController.java
+++ src/pis/hue2/server/ServerController.java
@@ -1,4 +1,16 @@
 package pis.hue2.server;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+
 public class ServerController {
+
+
+    @FXML
+    public Button startButton;
+    private TextArea messages;
+
+
+
 }
