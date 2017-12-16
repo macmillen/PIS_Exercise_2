763bb49a78eae19399f2d0ad07c3a7779905f032
dswn38 <dominik.schwan@mni.thm.de>
2017-12-16 13:42:21 +0100

    Server update

--- src/pis/hue2/server/ServerController.java
+++ src/pis/hue2/server/ServerController.java
@@ -3,12 +3,21 @@ package pis.hue2.server;
 
 import javafx.fxml.FXML;
 
+import java.lang.reflect.InvocationTargetException;
+
 
 public class ServerController {
 
 
     @FXML
     private void startServer() {
-        Main.server.start();
+
+        try {
+            Main.server.start();
+            System.out.println("Server started");
+        } catch (IllegalThreadStateException e) {
+            System.out.println("Server already started");
+
+        }
     }
 }
